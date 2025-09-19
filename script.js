a73e757a7caab59b774f9e9d529277cccd4bfd95
Suxxalotl <jaidanseto@gmail.com>
2025-09-19 09:26:38 -0400

    setup canvas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    const CANVAS = document.getElementById('canvas');
+    const gl = CANVAS.getContext('webgl');
+
+    if (gl == null){
+        alert('there was an issue initializing webGL. Your browser/machine might not support it.');
+        return;
+    }
+
+
+}
